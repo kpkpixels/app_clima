6869bfa87408bf15e1cd8bd1ee10949e9d325ad0
kpkpixels <pablobentz@gmail.com>
2023-05-08 16:22:52 -0300

    autocomplete cidades finalizado

--- js/index.js
+++ js/index.js
@@ -4,125 +4,172 @@ const campoBusca = document.querySelector(".campo-pesquisa button");
 const climaBox = document.querySelector(".clima-box");
 const climaDetalhes = document.querySelector(".clima-detalhes");
 const error404 = document.querySelector(".nao-encontrado");
+const autocompleteCidades = document.querySelector(".autocomplete-cidades");
 
 campoBusca.addEventListener("click", () => {
+  chamadaApi();
+});
+campoBuscaInput.addEventListener("keydown", function (e) {
+  if (e.code == "Enter") {
     chamadaApi();
+  }
 });
-campoBuscaInput.addEventListener("keydown", function(e) {
-    if (e.code == "Enter"){
-        chamadaApi();
-    }
-});
-campoBuscaInput.addEventListener("keydown", () => {
+
+campoBuscaInput.addEventListener("input", function (e) {
     matchMunicipio();
 });
 
-function chamadaApi(){
-    const APIKey = config.APIKey;
-    const cidade = document.querySelector(".campo-pesquisa input").value;
-  
-    if (cidade === "") {
-      return;
-    }
-  
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&lang=pt_br&APPID=${APIKey}`
-    ).then(result => result.json()).then
-    (json => {
-  
-      if (json.cod === "404"){
-          container.style.height = "400px";
-          climaBox.style.display = "none";
-          climaDetalhes.style.display = "none";
-          error404.style.display = "block";
-          error404.classList.add("fadeIn");
-          return;
+function chamadaApi() {
+  const APIKey = config.APIKey;
+  const cidade = document.querySelector(".campo-pesquisa input").value;
+
+  autocompleteCidades.classList.add("oculta-tela");
+
+  if (cidade === "") {
+    return;
+  }
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&lang=pt_br&APPID=${APIKey}`
+  )
+    .then((result) => result.json())
+    .then((json) => {
+      if (json.cod === "404") {
+        climaBox.classList.add("oculta-tela");
+        climaDetalhes.classList.add("oculta-tela");
+        error404.classList.remove("oculta-tela");
+        error404.style.display = "block";
+        error404.classList.add("fadeIn");
+        return;
       }
-  
-      error404.style.display = "none";
       error404.classList.remove("fadeIn");
-  
+
       const image = document.querySelector(".clima-box img");
       const temperatura = document.querySelector(".clima-box .temperatura");
-      const temperaturaMax = document.querySelector(".clima-box .temperaturaMax");
-      const temperaturaMin = document.querySelector(".clima-box .temperaturaMin");
+      const temperaturaMax = document.querySelector(
+        ".clima-box .temperaturaMax"
+      );
+      const temperaturaMin = document.querySelector(
+        ".clima-box .temperaturaMin"
+      );
       const descricao = document.querySelector(".clima-box .descricao");
       const umidade = document.querySelector(".clima-detalhes .umidade span");
       const vento = document.querySelector(".clima-detalhes .vento span");
-      
+
       console.log(json);
 
-      switch (json.weather[0].main){
-          case "Clear":{
-              image.src = "imagens/limpo.png";
-              break;
-          }
-          case "Rain":{
-              image.src = "imagens/chuvoso.png";  
-              break;
-          }
-          case "Snow":{
-              image.src = "imagens/neve.png"; 
-              break;
-          }
-          case "Clouds":{
-              image.src = "imagens/nuvens.png";
-              break;
-          }
-          case "Haze":{
-              image.src = "imagens/nublado.png";
-              break;
-          }
-          case "Fog":{
-            image.src = "imagens/nublado.png";
-            break;
+      switch (json.weather[0].main) {
+        case "Clear": {
+          image.src = "imagens/limpo.png";
+          break;
         }
-          default:
-              image.src = "";
-  
-  
+        case "Rain": {
+          image.src = "imagens/chuvoso.png";
+          break;
+        }
+        case "Snow": {
+          image.src = "imagens/neve.png";
+          break;
+        }
+        case "Clouds": {
+          image.src = "imagens/nuvens.png";
+          break;
+        }
+        case "Haze": {
+          image.src = "imagens/nublado.png";
+          break;
+        }
+        case "Fog": {
+          image.src = "imagens/nublado.png";
+          break;
+        }
+        default:
+          image.src = "";
       }
-  
+
       temperatura.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-      temperaturaMax.innerHTML = `${parseInt(json.main.temp_max)}°C  <i class="fas fa-arrow-up"></i>`;
-      temperaturaMin.innerHTML = `${parseInt(json.main.temp_min)}°C  <i class="fas fa-arrow-down"></i>`;
+      temperaturaMax.innerHTML = `${parseInt(
+        json.main.temp_max
+      )}°C  <i class="fas fa-arrow-up"></i>`;
+      temperaturaMin.innerHTML = `${parseInt(
+        json.main.temp_min
+      )}°C  <i class="fas fa-arrow-down"></i>`;
       descricao.innerHTML = `${json.weather[0].description}`;
       umidade.innerHTML = `${json.main.humidity}%`;
       vento.innerHTML = `${parseFloat(json.wind.speed)} Km/h`;
-  
+
       campoBuscaInput.value = campoBuscaInput.value + " - " + json.sys.country;
-  
+
+      climaBox.classList.remove("oculta-tela");
+      climaDetalhes.classList.remove("oculta-tela");
+
       climaBox.style.display = "";
       climaDetalhes.style.display = "";
       climaBox.classList.add("fadeIn");
       climaDetalhes.classList.add("fadeIn");
       container.style.height = "590px";
-  
+
       //getMunicipios();
-  
     });
 }
 
-function matchMunicipio(){
-    // const filtrado = cidades.nomeCidades.filter(str => str.includes(campoBuscaInput.value));
-    // console.log(filtrado);
-    const primeirosResultados = [];
-    let cont = 0;
+function matchMunicipio() {
+    var b,
+    i,
+    val = campoBuscaInput.value;
 
-    for (let i = 0; i < cidades.nomeCidades.length && cont < 5; i++) {
-        if (cidades.nomeCidades[i].toLowerCase().includes(campoBuscaInput.value)) {
-            primeirosResultados.push(cidades.nomeCidades[i]);
-            cont++;
-        }
+  climaBox.classList.add("oculta-tela");
+  climaDetalhes.classList.add("oculta-tela");
+  error404.classList.add("oculta-tela");
+  autocompleteCidades.classList.remove("oculta-tela");
+
+  limparListaCidades();
+  if (!val) {
+    return false;
+  }
+  let cont = 0;
+  let valorInput = val.toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, ''); //retirando acentos e deixando uppercase
+
+  for (i = 0; i < cidades.nomeCidades.length && cont < 5; i++) {
+    let nomeCidade = cidades.nomeCidades[i].slice(0, val.length).toUpperCase().normalize('NFD').replace(/[\u0300-\u036f]/g, ''); //retirando acentos e deixando uppercase
+    if (nomeCidade == valorInput) {
+      b = document.createElement("SPAN");
+      b.setAttribute("class", "autocomplete-itens");
+      b.innerHTML = "<strong>" + cidades.nomeCidades[i].slice(0, val.length) + "</strong>";
+      b.innerHTML += cidades.nomeCidades[i].slice(val.length);
+      b.innerHTML += "<input type='hidden' value='" + cidades.nomeCidades[i] + "'>";
+      container.style.height = "calc("+container.style.height+" + 55px)";
+
+      b.addEventListener("click", function (e) {
+        campoBuscaInput.value = this.getElementsByTagName("input")[0].value;
+        
+        limparListaCidades();
+        chamadaApi();
+      });
+      autocompleteCidades.appendChild(b);
+      cont++;
     }
-    console.log(primeirosResultados);
+  }
 }
 
-function getMunicipios(){
-    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/municipios?orderBy=nome`).then(result => result.json()).then
-    (json => {
-        for (let i = 0; i < json.length; i++) {
-            console.log('"'+json[i].nome+'",');
-        }
+function getMunicipios() {
+  fetch(
+    `https://servicodados.ibge.gov.br/api/v1/localidades/municipios?orderBy=nome`
+  )
+    .then((result) => result.json())
+    .then((json) => {
+      for (let i = 0; i < json.length; i++) {
+        console.log('"' + json[i].nome + '",');
+      }
     });
+}
+
+
+function limparListaCidades() {
+  var x = document.getElementsByClassName("autocomplete-itens");
+  
+  while (x.length > 0) {
+    x[x.length - 1].parentNode.removeChild(x[x.length - 1]);
+  }
+  container.style.height = "105px";
 }
